1f7230b815c5dca7ae5ad2de4c6365f8bdccaedb
Ahmedkhedr2 <ahmedkhedrx1@gmail.com>
2025-02-26 21:40:51 +0200

    fix text issue and refactor code

--- lib/Views/widgets/CustomDraweItem.dart
+++ lib/Views/widgets/CustomDraweItem.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/models/DrawerItemModel.dart';
 
 class Customdraweitem extends StatelessWidget {
- const Customdraweitem({super.key, required this.draweritemmodel});
+  const Customdraweitem({super.key, required this.draweritemmodel});
   final Draweritemmodel draweritemmodel;
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: Icon(draweritemmodel.icon),
-      title: Text(draweritemmodel.text),
+      title: FittedBox(
+        alignment: Alignment.centerLeft,
+        fit: BoxFit.scaleDown,
+        child: Text(draweritemmodel.text)),
     );
   }
 }
--- lib/Views/widgets/HomeViewBody.dart
+++ lib/Views/widgets/HomeViewBody.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/Views/widgets/Adabtive_Layout.dart';
 import 'package:responsive_dashboard/Views/widgets/DesktopLayout.dart';
 import 'package:responsive_dashboard/Views/widgets/MobileLayout.dart';
 import 'package:responsive_dashboard/Views/widgets/TabletLayout.dart';
@@ -12,15 +13,11 @@ class Homeviewbody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: LayoutBuilder(builder: (context, constrains) {
-        if (constrains.maxWidth < 600) {
-          return MobileLayout();
-        } else if (constrains.maxWidth < 900) {
-          return TabletLayout();
-        } else {
-          return Desktoplayout();
-        }
-      }),
+      child: AdabtiveLayout(
+        MobileLayout: (context) => MobileLayout(),
+        TabletLayout: (context) => TabletLayout(),
+        DesktopLayout: (context) => Desktoplayout(),
+      ),
     );
   }
 }
