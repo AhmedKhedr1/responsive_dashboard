6326399cd8b4c29e7fbd477e1531737bb7f13023
Ahmedkhedr2 <ahmedkhedrx1@gmail.com>
2025-03-13 11:57:49 +0200

    add Style File have all text style

--- lib/Views/DashBoardView.dart
+++ lib/Views/DashBoardView.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/Widgets/AdabtiveLayoutWidget.dart';
+import 'package:responsive_dashboard/Widgets/DashboardDesktopLayout.dart';
 
 class Dashboardview extends StatelessWidget {
   const Dashboardview({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Adabtivelayoutwidget(
+        MobileLayout: (context) => SizedBox(),
+        Tabletayout: (context) => SizedBox(),
+        DesktopLayout: (context) => Dashboarddesktoplayout(),
+      ),
+    );
   }
 }
---
+++ lib/Widgets/DashboardDesktopLayout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Dashboarddesktoplayout extends StatelessWidget {
+  const Dashboarddesktoplayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
