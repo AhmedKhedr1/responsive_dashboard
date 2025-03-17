3c0cc18976b1c23184582d1955669bfcd2b7ac3c
Ahmedkhedr2 <ahmedkhedrx1@gmail.com>
2025-03-17 12:13:17 +0200

    Finish Customdrawer and fix errors

--- lib/Widgets/CustomDrawer.dart
+++ lib/Widgets/CustomDrawer.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/Widgets/DrawerItemsListView.dart';
+import 'package:responsive_dashboard/Widgets/InActiveDrawerItem.dart';
 import 'package:responsive_dashboard/Widgets/user_info_listtile.dart';
+import 'package:responsive_dashboard/models/Drawer_item_model.dart';
 import 'package:responsive_dashboard/utils/Appimages.dart';
 
 class Customdrawer extends StatelessWidget {
   const Customdrawer({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Column(
-        children: [
-          UserInfoListtile(
-            image: Assets.imagesAvatar3,
-            title: 'Lekan Okeowo',
-            subtitle: 'demo@gmail.com',
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UserInfoListtile(
+              image: Assets.imagesAvatar3,
+              title: 'Lekan Okeowo',
+              subtitle: 'demo@gmail.com',
+            ),
           ),
-          SizedBox(
-            height: 8,
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 8,
+            ),
           ),
-          DrawerItemsListView()
+          DrawerItemsListView(),
+          //  SliverToBoxAdapter(child: Expanded(child: SizedBox())),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(child: SizedBox()),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: 'Setting system', image: Assets.imagesSettings),
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: 'Logout account', image: Assets.imagesLogout),
+                ),
+                SizedBox(
+                  height: 48,
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
