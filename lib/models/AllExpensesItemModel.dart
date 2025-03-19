68e380579630386913625adc85500298d9c88cb6
Ahmedkhedr2 <ahmedkhedrx1@gmail.com>
2025-03-19 13:31:45 +0200

    add all expenses item model

---
+++ lib/models/AllExpensesItemModel.dart
@@ -0,0 +1,9 @@
+class Allexpensesitemmodel {
+  final String image, title, date;
+  final double price;
+  Allexpensesitemmodel(
+      {required this.image,
+      required this.title,
+      required this.date,
+      required this.price});
+}
