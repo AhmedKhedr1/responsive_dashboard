ae26e2c01a2f2308b12deea66c37a07074a0d407
Ahmedkhedr2 <ahmedkhedrx1@gmail.com>
2025-04-08 23:22:53 +0200

    add custom dot

---
+++ lib/Widgets/Custom_dot.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomDot extends StatelessWidget {
+  const CustomDot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
